3a261fd11943fe866fcc2e77dc91156ef8e56721
userliluzibert <57073805+userliluzibert@users.noreply.github.com>
2024-04-25 19:48:51 -0700

    attempt to fix skill bar percentages

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -1,26 +1,29 @@
 const allPanels = Array.from(document.querySelectorAll(".panel"));
 const allAccordion = Array.from(document.querySelectorAll(".accordion"));
+
+
+// Set all panels to expanded by default
+allPanels.forEach(function (panel) {
+  panel.style.maxHeight = panel.scrollHeight + "px";
+});
+
 const expandAccordion = (elem) => {
-  if (!elem.parentElement.classList.contains("active")) {
-    allAccordion.forEach((acc) => {
-      acc.classList.remove("active");
-    });
-    elem.parentElement.classList.add("active");
-    allPanels.forEach(function (elem) {
-      elem.style.maxHeight = null;
-    });
-    let activePanel = elem.parentElement.nextElementSibling;
-    if (
-      activePanel.id != "skill-panel" &&
-      document.querySelector("#skill-panel")
-    ) {
-      let skillBars = Array.from(document.querySelectorAll("#skill-percent"));
-      skillBars.forEach((elem) => {
-        elem.style.width = "0";
-      });
+  let panel = elem.parentElement.nextElementSibling;
+  
+  // Toggle the 'active' class on the accordion element
+  //elem.parentElement.classList.toggle("active");
+  
+  // If the panel is already open, close it
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    // Open the panel
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    // If this is the skill panel, initialize the skill bars
+    if (panel.id === "skill-panel") {
+      //MAKE SKILL PANEL SHOW PERCENTAGES BY DEFAULT
+      panel.scrollHeight + "px"
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
-    activePanel.style.maxHeight = activePanel.scrollHeight + "px";
   }
 };
-
-
