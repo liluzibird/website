5faf9e1bd799be6cd349318ebc70bf579df72692
userliluzibert <57073805+userliluzibert@users.noreply.github.com>
2024-04-25 19:55:09 -0700

    change base url link

---
+++ public/js/accordion.js
@@ -0,0 +1,29 @@
+const allPanels = Array.from(document.querySelectorAll(".panel"));
+const allAccordion = Array.from(document.querySelectorAll(".accordion"));
+
+
+// Set all panels to expanded by default
+allPanels.forEach(function (panel) {
+  panel.style.maxHeight = panel.scrollHeight + "px";
+});
+
+const expandAccordion = (elem) => {
+  let panel = elem.parentElement.nextElementSibling;
+  
+  // Toggle the 'active' class on the accordion element
+  //elem.parentElement.classList.toggle("active");
+  
+  // If the panel is already open, close it
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    // Open the panel
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    // If this is the skill panel, initialize the skill bars
+    if (panel.id === "skill-panel") {
+      //MAKE SKILL PANEL SHOW PERCENTAGES BY DEFAULT
+      panel.scrollHeight + "px"
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+};
